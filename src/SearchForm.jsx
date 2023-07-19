6514f16e211127ef1962a6a2b122e7a6787300d8
AayushGarg-1905 <111649544+AayushGarg-1905@users.noreply.github.com>
2023-07-19 11:13:56 +0530

    Update SearchForm.jsx

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -6,11 +6,6 @@ const SearchForm = () => {
   const {setSearchTerm}=useGlobalContext()
     const handleSubmit=(e)=>{
         e.preventDefault();
-
-        // here we access the all the form components using e.target.elements 
-        // now we can see the result by logging this.
-        // to access the input value there is a property of search(this is value we set in the name attributre of input element) inside which has a property value => this contains the value enetered by user
-        // console.log(e.target.elements)
         const searchValue=e.target.elements.search.value;
         if(!searchValue){
           return;
